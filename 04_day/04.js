004ce1b2bf1de2d70cbf7d0727f311a2e2e1d10e
hive-cmky <6779431+hive-cmky@users.noreply.github.com>
2024-07-28 13:12:24 +0000

    activity 1 comple of for loop done.

---
+++ 04_day/04.js
@@ -0,0 +1,12 @@
+// TASKS/ACTIVITIES:
+// Activity 1: For Loop
+// Task 1: Write a program to print numbers from 1 to 10 using for Loop
+
+for (let i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+// Task 2: Write a program to print multiplication table of 5 using a for loop
+for (let i = 1; i <= 10; i++){
+    console.log( 5 * i);
+}
