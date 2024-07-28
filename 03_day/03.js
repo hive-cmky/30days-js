a02522d5137695af1a5c5660d44ccb4ef731d1f5
hive-cmky <6779431+hive-cmky@users.noreply.github.com>
2024-07-28 12:37:17 +0000

    day 3 task done.

--- 03_day/03.js
+++ 03_day/03.js
@@ -33,14 +33,77 @@ console.log("The largest number is " + largest);
 // Activity 3: switch case
 // Task 4: write a program that uses a switch case to determine
 // the day of the week based on a number (1-7) and log the day name to the console.
-// Task 5: write a program that uses a switch case to assign a grade ('A', 'B', 'C', 'D', 'F')based on a score and log the grade to the console.
+let day =3;
+let dayName;
+switch(day) {
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    case 7:
+        dayName = "Sunday";
+        break;
+    default:
+        dayName = "Invalid Day";
+}
+console.log(dayName);
+// Task 5: write a program that uses a switch case to assign a grade ('A', 'B', 'C', 'D', 'F')based on a score and log the 
+// grade to the console.
+
+
+let grade = "W";
+
+switch (grade) {
+    case "A":
+        console.log("Your score is between 90 to 100.");
+        break;
+    case "B":
+        console.log("Your score is between 80 to 90");
+        break;
+    case "C":
+        console.log("Your score is between 70 to 80");
+        break;
+    case "D":
+        console.log("Your score is between 60 to 70");
+    case "F":
+        console.log("Your score is between 50 to 60");
+    default:
+        console.log("Invalid score.");
+}
+
 
 // Activity 4: conditional (Ternary) operator
 // Task 6: write a program that uses the ternary operator to check if a number is even or odd and log the result to the console.
+let number = 10;
+console.log(number % 2 === 0 ? "number is even" : "number is odd");
 
 // Activity 5: Combining conditions
-// Task 7: write a program to check if a year is a leap year using multiple conditions (divisible by 4, but not 100 unless also divisible by 400) and log the result to the console.
+// Task 7: write a program to check if a year is a leap year using multiple conditions 
+// (divisible by 4, but not 100 unless also divisible by 400) and log the result to the console.
 
+function checkLeapYear(year) {
+    if ( (0 === year % 4) && (0 !== year % 100 || 0 === year % 400)  ){
+        console.log(year + " is a leap year.")
+    }else{
+        console.log(year + " is not a leap year.");
+    }
+}
+const year = prompt("Enter the year: ");
+checkLeapYear(year);
 // Feature Request:
 // 1. Number Check Script: write a script that checks if a number is positive, negative or zero using if-else statements and logs the result.
 // 2. voting eligibility scirpt: create a script to check if a person is eligible to vote based on their age and log the result.
