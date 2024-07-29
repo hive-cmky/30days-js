ea7ead7b5c817b64c520a6f16c831e0682005e12
hive-cmky <6779431+hive-cmky@users.noreply.github.com>
2024-07-29 08:30:50 +0000

    Task 3 done.

--- 04_day/04.js
+++ 04_day/04.js
@@ -10,3 +10,15 @@ for (let i = 0; i <= 10; i++){
 for (let i = 1; i <= 10; i++){
     console.log( 5 * i);
 }
+
+// Activity 2: While loop
+// Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a While loop
+let i = 1, sum = 0;
+while( i < 10){
+    sum += i;
+    i++;
+}
+console.log(sum);
+
+// Task 4: Write a program to print numbers from 10 to 1 using while loop
+
