5d26e45b71cf0cd80a781da83336108418f0a00b
hive-cmky <6779431+hive-cmky@users.noreply.github.com>
2024-07-31 08:02:47 +0000

    06 and 17 js added to the folder.

---
+++ 06_day/06.js
@@ -0,0 +1,11 @@
+// Tasks/Activities
+
+// Activity 1: Array creation and access
+// Task 1: create an array of numbers from 1 to 5 and log the array to the console.
+// Task 2: Access the first and last elements of the array and log them to the console.
+
+// Activity 2: Array Methods(Basic)
+// Task 3: Use the push method to add a new number to the end of the array and log the updated array.
+// Task 4: Use the pop method to remove the last element from the array and log the updated array.
+// Task 5: Use the shift method to remove the first element from the array and log the updated array.
+// Task 6: Use the unshift method to add a new number to the beginning of the array and log the updated array.
